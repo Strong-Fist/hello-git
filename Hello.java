a65194318a9cb91f9eb23c2ea894a8429aceef5a
EC2 Default User <ec2-user@ip-172-31-1-241.ap-northeast-2.compute.internal>
2024-12-06 12:52:07 +0000

    added license to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,3 +1,8 @@
+/*
+ * 안녕하세요 방갑습니다. 
+ * 이것은 git workflow을 실습하기 위해서 작성하는 내요입니다.
+ * 모두 행복하시고 건강하세요
+*/
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
